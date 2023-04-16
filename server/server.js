90eb16fd8af57dc54ab222a8765c8bf572b36063
Circulartextapp <musicworldtakeover@gmail.com>
2023-04-16 17:58:45 -0400

    first commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
-const PORT = process.env.PORT || 5000;
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -56,3 +55,5 @@ app.post('/', async (req, res) => {
 })
 
 
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+const PORT = process.env.PORT || 5000;
